f6983ab3cbf96072b26121f5314bac93003fdca5
Ben Sully <ben.sully@grafana.com>
2023-07-10 22:32:25 +0100

    Upgrade shuttle and some other things

--- crates/recipe-shuttle/src/main.rs
+++ crates/recipe-shuttle/src/main.rs
@@ -1,5 +1,3 @@
-use sync_wrapper::SyncWrapper;
-
 use recipe_db::DbPool;
 use recipe_graphql::create_schema;
 use recipe_server::{create_router, AppState};
@@ -7,18 +5,17 @@ use recipe_server::{create_router, AppState};
 mod pg_pool;
 mod service_info;
 
-#[shuttle_service::main]
+#[shuttle_runtime::main]
 async fn axum(
     #[pg_pool::ShuttleDbPool] pool: DbPool,
-    #[service_info::ShuttleServiceInfo] service_info: service_info::ServiceInfo,
-) -> shuttle_service::ShuttleAxum {
+    // #[service_info::ShuttleServiceInfo] service_info: service_info::ServiceInfo,
+) -> shuttle_axum::ShuttleAxum {
     let schema = create_schema(pool);
     let api_url = Box::leak(Box::new(format!(
-        "https://{}.shuttleapp.rs/graphql",
-        service_info.name
+        "https://recipes.shuttleapp.rs/graphql",
+        // service_info.name
     )));
     let router = create_router().with_state(AppState::new(schema).with_api_url(api_url.as_str()));
-    let sync_wrapper = SyncWrapper::new(router);
 
-    Ok(sync_wrapper)
+    Ok(router.into())
 }
