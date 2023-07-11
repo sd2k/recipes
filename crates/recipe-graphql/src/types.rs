168703b1ceae21eb3749dc27efed845ee170cb7a
Ben Sully <ben.sully@grafana.com>
2023-07-11 12:48:01 +0100

    Add GraphQL query to scrape a recipe; add types

--- crates/recipe-graphql/src/types.rs
+++ crates/recipe-graphql/src/types.rs
@@ -2,8 +2,10 @@ mod ingredient;
 mod measurement;
 mod recipe;
 mod recipe_ingredient;
+mod scraped_recipe;
 
 pub use ingredient::*;
 pub use measurement::*;
 pub use recipe::*;
 pub use recipe_ingredient::*;
+pub use scraped_recipe::*;
