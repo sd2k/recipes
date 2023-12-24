7488cbc9a4f28cfd1eb55216aa6a3afa763d1dc7
Ben Sully <ben.sully@grafana.com>
2023-12-24 13:27:03 +0000

    Use Router in desktop

--- crates/recipe-desktop/src/main.rs
+++ crates/recipe-desktop/src/main.rs
@@ -2,7 +2,7 @@ use dioxus_desktop::{Config, WindowBuilder};
 use dioxus_fullstack::prelude::*;
 use log::LevelFilter;
 
-use recipe_app::app;
+use recipe_app::Route;
 
 fn main() {
     server_fn::set_server_url("http://127.0.0.1:8080");
@@ -10,5 +10,7 @@ fn main() {
     let config = Config::new()
         .with_custom_head(r#"<link rel="stylesheet" href="public/tailwind.css">"#.to_string())
         .with_window(WindowBuilder::new().with_title("Recipes"));
-    LaunchBuilder::new(app).desktop_cfg(config).launch()
+    LaunchBuilder::<FullstackRouterConfig<Route>>::router()
+        .desktop_cfg(config)
+        .launch()
 }
